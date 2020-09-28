7eabb0c4be79af651d814c75b24e26953117d323
shekhar maheswari <shekharmaheswari85@gmail.com>
2020-09-28 16:21:01 +0600

    Final Commit

--- adoption_site.py
+++ adoption_site.py
@@ -2,7 +2,7 @@ import os
 from flask import Flask, render_template, url_for, redirect
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
-from forms import AddForm, DelForm
+from forms import AddForm, DelForm, OwnerForm
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'mysecretkey'
@@ -18,17 +18,47 @@ class Puppy(db.Model):
     __tablename__ = 'puppies'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.Text)
+    owner = db.relationship('Owner', backref='Puppy', uselist=False)
 
     def __init__(self, name):
         self.name = name
 
     def __repr__(self):
-        return f"Puppy name is: {self.name}"
+        if self.owner:
+            return f"Puppy name is {self.name} and owner is {self.owner.name}"
+        return f"Puppy name is: {self.name} and no owner has been assigned yet!"
+
+
+class Owner(db.Model):
+    __tablename__ = 'owners'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.Text)
+    puppy_id = db.Column(db.Integer, db.ForeignKey('puppies.id'))
+
+    def __init__(self, name, puppy_id):
+        self.name = name
+        self.puppy_id = puppy_id
+
+    def __repr__(self):
+        return f"Owner name : {self.name}"
 
 
 @app.route('/')
 def index():
     return render_template('home.html')
+
+
+@app.route('/add_owner', methods=['GET', "POST"])
+def add_owner():
+    form = OwnerForm()
+    if form.validate_on_submit():
+        name = form.name.data
+        pup_id = form.pup_id.data
+        new_owner = Owner(name, pup_id)
+        db.session.add(new_owner)
+        db.session.commit()
+        return redirect(url_for('list_pup'))
+    return render_template('add_owner.html', form=form)
 
 
 @app.route('/add', methods=['GET', 'POST'])
--- forms.py
+++ forms.py
@@ -10,3 +10,9 @@ class AddForm(FlaskForm):
 class DelForm(FlaskForm):
     id = IntegerField("Id Number of the Puppy to Remove:")
     submit = SubmitField("Remove Puppy")
+
+
+class OwnerForm(FlaskForm):
+    name = StringField("Name of Owner: ")
+    pup_id = IntegerField("Id of Puppy: ")
+    submit = SubmitField("Add Owner")
